38eeebb347d144e19ff5934d89a1079c2bd25664
yogibaba7 <yogeshchouhan263@gmail.com>
2026-04-29 12:18:24 +0530

    removed mul and div module

--- app.py
+++ app.py
@@ -7,14 +7,9 @@ def sum(a, b):
 def sub(a, b):
     return a-b
 
-def multiply(a,b):
-    return a*b
 
-def division(a,b):
-    return a/b
 
 r1 = sum(2, 3)
 r2 = sub(4, 5)
-r3 = multiply(2,3)
-r4 = division(30,4)
-print(r1, r2, r3,r4)
+
+print(r1, r2)
