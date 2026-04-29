32943ba555cb0606561064ed124f7eb79d6b6854
yogibaba7 <yogeshchouhan263@gmail.com>
2026-04-29 12:39:27 +0530

    error resolved

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ def sum(a, b):
 def sub(a, b):
     return a-b
 
-def mul(a,b):
+def mul(a, b):
     return a*b
 
-def div(a,b):
+def div(a, b):
     return a/b
 
 r1 = sum(2, 3)
 r2 = sub(4, 5)
-r3 = mul(4,6)
-r4 = div(5/2)
+r3 = mul(4, 6)
+r4 = div(5/ 2)
 
-print(r1, r2,r3,r4)
+print(r1, r2, r3, r4)
