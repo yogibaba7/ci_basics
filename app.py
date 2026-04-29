68840900b3e24259122cd12757de701743626fe7
yogibaba7 <yogeshchouhan263@gmail.com>
2026-04-29 09:14:05 +0530

    dkla

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # just basic code
 
-def sum(a,b):
+def sum(a, b):
     return a+b
 
-def sub(a,b):
+
+def sub(a, b):
     return a-b
 
-r1 = sum(2,3)
-r2 = sub(4,5)
+
+r1 = sum(2, 3)
+r2 = sub(4, 5)
 
 print(r1,r2)
