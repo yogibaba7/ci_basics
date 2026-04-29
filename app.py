81356239ec0fc69a43b55bbdfff8508c2874c588
yogibaba7 <yogeshchouhan263@gmail.com>
2026-04-29 12:41:15 +0530

    error resolve1

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # just basic code
 
 def sum(a, b):
-    return a+b
+    return a + b
 
 
 def sub(a, b):
-    return a-b
+    return a - b
 
 def mul(a, b):
-    return a*b
+    return a * b
 
 def div(a, b):
-    return a/b
+    return a / b
 
 r1 = sum(2, 3)
 r2 = sub(4, 5)
