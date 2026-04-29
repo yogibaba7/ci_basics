1ad779e1000a03ff0942587be95bb78002cd2665
yogibaba7 <yogeshchouhan263@gmail.com>
2026-04-29 12:12:57 +0530

    added division module

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ def sub(a, b):
 def multiply(a,b):
     return a*b
 
+def division(a,b):
+    return a/b
+
 r1 = sum(2, 3)
 r2 = sub(4, 5)
 r3 = multiply(2,3)
-
-print(r1, r2, r3)
+r4 = division(30,4)
+print(r1, r2, r3,r4)
